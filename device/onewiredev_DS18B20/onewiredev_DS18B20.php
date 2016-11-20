82fda0a476a65fcf7cc56716cada300cb52130e7
Lekster <you@example.com>
2016-11-20 20:24:47 +0300

    fix

--- device/onewiredev_DS18B20/onewiredev_DS18B20.php
+++ device/onewiredev_DS18B20/onewiredev_DS18B20.php
@@ -1,1 +1,58 @@
-/home/asmirnov/points/pbr-wifc-asm-test/pbr-wifc-asm-test//plugins/onewiredevices/device//onewiredev_DS18B20/onewiredev_DS18B20.php
+<?php
+
+require_once (PROJ_DIR . "/htdocs/console/controllers/AbstractDevice.php");
+
+class onewiredev_DS18B20 extends AbstractDevice
+{
+	
+	protected $MNT_DIR = '/mnt/1wire/';
+	protected $TYPES = ['DS18B20'];
+
+	protected function findAll1wireDevAddr()
+	{
+		$str = "find {$this->MNT_DIR} -maxdepth 1  -type d  | grep -E '[0-9A-F]{2}\.'";
+		$ret = [];
+		exec($str, $ret);
+		return $ret;
+	} 
+
+	public function discovery()
+	{
+		$ret = [];
+		$paths = $this->findAll1wireDevAddr();
+		foreach ($paths as $path)
+		{
+			$addr = substr($path, strrpos($path, "/") + 1);
+			$type = exec("cat {$this->MNT_DIR}/$addr/type");
+			if (in_array($type, $this->TYPES))
+				$ret[] = $addr;
+		}
+		return $ret;
+	}
+	
+	public function getPortsConf()
+	{
+		return array(
+			'a0' => ['AccessType' => 'R', 'PortReal' => 'temperature10'],
+		);
+	}
+
+	protected function setPortValTempl($port, $val)
+	{
+		return true;
+	}
+
+	protected function getPortValTempl($port)
+	{
+		$str = "cat {$this->MNT_DIR}/{$this->_addr}/$port";
+		return exec($str);
+	}
+	
+	public function ping()
+	{
+		return true;
+	}
+
+	public function getVersion() {return "0.0.1"; }
+
+}
