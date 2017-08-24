01ed2604db35a1b093d0724d347628a37c4a22a6
asmirnov <asmirnov@immo.ru>
2017-08-24 18:04:19 +0300

    dev

--- device/onewiredev_DS18B20/onewiredev_DS18B20.php
+++ device/onewiredev_DS18B20/onewiredev_DS18B20.php
@@ -1,99 +1,25 @@
 <?php
 
 use console\controllers\AbstractDevice;
+use src\helpers\SysHelper;
 
-require_once (__DIR__ . "/../../src/ownet.php");
+
+require_once (__DIR__ . "/../../src/onewiredev.php");
 //!!! sudo apt-get install php-bcmath
 
-class onewiredev_DS18B20 extends AbstractDevice
+class onewiredev_DS18B20 extends OneWireDevice
 {
 	
-	protected $MNT_DIR = '/mnt/1wire/';
-	protected $TYPES = ['DS18B20'];
-
-	protected function findAll1wireDevAddr()
-	{
-		$ow=new OWNet("tcp://localhost:4304");
-		$data = $ow->dir("/");
-		$devs =  array_map(function ($x) { return str_replace("/", "", $x) ;}, explode(",", $data['data']));
-		$devs = array_filter($devs, function ($x) { return preg_match("/^[0-9A-F]{2}\./i", $x); });
-
-		//$my_value = $ow->get("28.D6C18D020000/temperature");
-		$ret = $devs;
-
-
-		/*$str = "find {$this->MNT_DIR} -maxdepth 1  -type d  | grep -E '[0-9A-F]{2}\.'";
-		var_dump($str);
-		$ret = [];
-		exec($str, $ret);
-		*/
-		return $ret;
-	} 
-
-	public function discovery()
-	{
-		$ret = [];
-		try {
-			$paths = $this->findAll1wireDevAddr();
-			foreach ($paths as $path)
-			{
-				$addr = $path;
-				//$addr = substr($path, strrpos($path, "/") + 1);
-				$ow=new OWNet("tcp://localhost:4304");
-				$type = $ow->get("$addr/type");
-				//$type = exec("cat {$this->MNT_DIR}/$addr/type");
-				if (in_array($type, $this->TYPES))
-					$ret[] = new onewiredev_DS18B20 ($addr);
-			}	
-		} catch (Exception $e) {
-			Yii::error("Exception|" . $e->getMessage());
-		}
-		
-		return $ret;
-	}
+	const TYPES = ['DS18B20'];
 	
 	public function getPortsConf()
 	{
 		return array(
-			'a0' => ['AccessType' => 'R', 'PortReal' => 'temperature10'],
+			'temperature' => ['AccessType' => 'R', 'PortReal' => 'temperature10'],
 		);
 	}
 
-	protected function setPortValTempl($port, $val)
-	{
-		return true;
-	}
-
-	protected function getPortValTempl($port)
-	{
-		//$str = "cat {$this->MNT_DIR}/{$this->_addr}/$port";
-		//return exec($str);
-		try {
-			$ow=new OWNet("tcp://localhost:4304");
-			return $ow->get("{$this->_addr}/$port");
-		} catch (Exception $e) {
-			Yii::error("Exception|" . $e->getMessage());		
-			return null;
-		}
-		
-	}
-	
-	public function ping()
-	{
-		return true;
-	}
 
 	public function getVersion() {return "0.0.1"; }
 
-	public function getMacAddress()
-	{
-		return $this->_addr;
-	}
-
-	public function getOptions()
-	{
-		
-	}
-
-	public function setOptions(array $opt) {}
 }
---
+++ src/onewiredev.php
@@ -0,0 +1,124 @@
+<?php
+
+use console\controllers\AbstractDevice;
+use src\helpers\SysHelper;
+
+require_once (__DIR__ . "/ownet.php");
+//!!! sudo apt-get install php-bcmath
+
+
+abstract class OneWireDevice extends AbstractDevice
+{
+	
+	protected static $owserver_conn_str;
+	protected static $class_types = [];
+
+
+	public static function discovery()
+	{
+
+		$files = SysHelper::glob_recursive(__DIR__ . "/../device/*.php");
+
+		//get classes for types
+		foreach ($files as $fileName)
+		{
+			$classes = get_declared_classes();
+			if (file_exists($fileName)) 
+			{ 
+				require_once $fileName;
+				$className = str_replace(".php", "",  substr($fileName, strripos("$fileName", "/") + 1));
+				//$a = new $className;
+				//if ($a instanceof \AbstractDevice)
+				$rcl = @new \ReflectionClass($className);
+				if (!$rcl->isAbstract() && $rcl->isSubclassOf("console\controllers\AbstractDevice"))
+				{
+					foreach ($rcl->getConstant("TYPES") as $type )
+					{
+						self::$class_types[$type] = $className;
+					}	
+				}
+				
+			}
+		}
+
+		//find devices
+		$ret = [];
+		try {
+			$paths = self::findAll1wireDevAddr();
+			foreach ($paths as $path)
+			{
+				$addr = $path;
+				//$addr = substr($path, strrpos($path, "/") + 1);
+				$ow=new OWNet(self::$owserver_conn_str);
+				$type = $ow->get("$addr/type");
+				//$type = exec("cat /mnt/1wire/$addr/type");
+				$ret[] = new self::$class_types[$type] ($addr);
+			}	
+		} catch (Exception $e) {
+			Yii::error("Exception|" . $e->getMessage());
+		}
+		//getStaticPropertyValue
+		return $ret;
+	}
+
+	protected static function findAll1wireDevAddr()
+	{
+		$ow=new OWNet(self::$owserver_conn_str);
+		$data = $ow->dir("/");
+		$devs =  array_map(function ($x) { return str_replace("/", "", $x) ;}, explode(",", $data['data']));
+		$devs = array_filter($devs, function ($x) { return preg_match("/^[0-9A-F]{2}\./i", $x); });
+		$ret = $devs;
+
+		/*$str = "find {$this->MNT_DIR} -maxdepth 1  -type d  | grep -E '[0-9A-F]{2}\.'";
+		var_dump($str);
+		$ret = [];
+		exec($str, $ret);
+		*/
+		return $ret;
+	} 
+
+	protected function init()
+	{
+		$owserver_host = SysHelper::getPluginSetting('onewiredevices', "owserver_host");
+		$owserver_port = SysHelper::getPluginSetting('onewiredevices', "owserver_port");
+		self::$owserver_conn_str = "tcp://$owserver_host:$owserver_port";
+	}
+
+	protected function setPortValTempl($port, $val)
+	{
+		try {
+			$ow=new OWNet(self::$owserver_conn_str);
+			return $ow->set("{$this->_addr}/$port");
+		} catch (Exception $e) {
+			Yii::error("Exception|" . $e->getMessage());		
+			return false;
+		}
+	}
+
+	protected function getPortValTempl($port)
+	{
+		//$str = "cat {$this->MNT_DIR}/{$this->_addr}/$port";
+		//return exec($str);
+		try {
+			$ow=new OWNet(self::$owserver_conn_str);
+			return $ow->get("{$this->_addr}/$port");
+		} catch (Exception $e) {
+			Yii::error("Exception|" . $e->getMessage());		
+			return null;
+		}
+		
+	}
+
+	public function getOptions()
+	{
+		
+	}
+
+	public function setOptions(array $opt) {}
+
+	public function ping()
+	{
+		return true;
+	}
+
+}
